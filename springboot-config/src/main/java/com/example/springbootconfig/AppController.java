9dd76467f316ff9668f76c69d9835d38a190c54e
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2020-04-03 12:10:35 -0300

    Correctin and finalize the application

--- springboot-config/src/main/java/com/example/springbootconfig/AppController.java
+++ springboot-config/src/main/java/com/example/springbootconfig/AppController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.springbootconfig.config.DBConfiguration;
+
 @RestController
 public class AppController {
 
