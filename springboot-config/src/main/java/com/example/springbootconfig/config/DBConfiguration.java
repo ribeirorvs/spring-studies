fb483605e43fdd6201315845b8323ae8a68e21ca
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2020-04-03 01:04:54 -0300

    Add lombok project and getter and setter

--- springboot-config/src/main/java/com/example/springbootconfig/config/DBConfiguration.java
+++ springboot-config/src/main/java/com/example/springbootconfig/config/DBConfiguration.java
@@ -5,8 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Configuration
 @ConfigurationProperties("spring.datasource")
+@Getter
+@Setter
 public class DBConfiguration {
 	
 	private String driverClassName;
